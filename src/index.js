3404440658cf24c5392d51bf89c41400b865f6a1
Eric <eric@ericyd.com>
2017-07-15 15:52:33 -0700

    add sidebar push menu animations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import App from './components/App';
 
+// clear spinner
+document.getElementById('app').innerHTML = '';
+
 const app = new App({
   target: document.getElementById('app'),
   data: {},
